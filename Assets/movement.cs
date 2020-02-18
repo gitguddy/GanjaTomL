25b89982d7b3dc6004e9d245ed22c35013c9a9bb
Kley <Lorenz.Kley@stud.fh-rosenheim.de>
2020-02-18 19:36:38 +0100

    First session

---
+++ Assets/movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isInAir",true);
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        //Move your Charakter
+    }
+
+    public void OnLanding(){
+        animator.SetBool("isJumping", false);
+    }
+}
